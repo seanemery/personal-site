07d212126e60c37ee4b18463e17b0bab6f62cc87
seanemery <sean@emerynet.com>
2020-10-30 22:46:02 -0400

    add static content and fix eb config

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -37,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
