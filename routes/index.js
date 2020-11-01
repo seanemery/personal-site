0d3fa181f4a5f5f9ee03ab91bcef8d496e21b003
seanemery <sean@emerynet.com>
2020-10-31 23:58:49 -0400

    add a guide to the homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , hint: 'Don\'t move your pawns ever'});
+  res.render('index', { title: 'Express' , hint: 'Don\'t move your pawns ever', guide: path.join(".", 'images', 'your_guide.png')});
 });
 
 module.exports = router;
