0aabf832ab0f2bda3e69b683a592230f752678fe
seanemery <sean@emerynet.com>
2020-11-03 22:19:53 -0500

    draw an 8x8 chess board on the new chess page!

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chessRouter = require('./routes/chess');
 
 var app = express();
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chess', chessRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
